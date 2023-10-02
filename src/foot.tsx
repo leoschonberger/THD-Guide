451c12773668f431460320dd3c5c48aa9abba7df
Leo Schonberger <leoschonberger@gmail.com>
2023-10-01 20:20:03 -0700

    fixed footer link issue

--- src/foot.tsx
+++ src/foot.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 z-20 w-full border-t border-gray-200 bg-white p-4 shadow dark:border-gray-600 dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6">
@@ -8,12 +10,12 @@ export default function Footer() {
         </a>
         . All Rights Reserved.
       </span>
-      <a
+      <Link
         href="/"
         className="flex flex-wrap text-center text-white hover:underline"
       >
         Start Over
-      </a>
+      </Link>
       <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a href="#" className="mr-4 hover:underline md:mr-6">
